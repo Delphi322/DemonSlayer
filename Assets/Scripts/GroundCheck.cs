fa24ed8e4e86ea185e1749dc0cf5565799ecc348
Blossom13 <rawlinson322@gmail.com>
2021-03-16 12:14:11 +0000

    made player move and jump scripts

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+
+    public PlayerController player;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.isGrounded = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.isGrounded = false;
+        }
+    }
+    private void Update()
+    {
+        transform.position = player.gameObject.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float speed;
+
+    public float jump;
+
+    public bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        if(isGrounded)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float moveBy = x * speed;
+            rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+    }
+}
