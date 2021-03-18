779952ad4782f22b8c0daa1186443bb0720eeccc
Blossom13 <rawlinson322@gmail.com>
2021-03-18 16:13:25 +0000

    Added several hud elements, axe and sword sub weapons with animation, spikes that kill you in one hit and the health system

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public Image[] healthImgs;
+    PlayerHealth playerHealth;
+    int health;
+
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+        health = playerHealth.health;
+
+        for (int i = 0; i <= healthImgs.Length - 1; i++)
+        {
+            if (i < health) healthImgs[i].sprite = sprites[0];
+            
+            else healthImgs[i].sprite = sprites[1];
+            
+        }
+    }
+}
