ad17874e1706bde209c264fa541f8a83cbe6638a
Blossom13 <rawlinson322@gmail.com>
2021-03-23 12:14:43 +0000

    Walk left and right animations added and work, other animations added but not working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,17 @@ public class PlayerController : MonoBehaviour
 
     public bool isGrounded;
 
+    Animator anim;
+
+    public Vector2 lastMove;
+    public float ClampMoveX;
+    private Vector2 moveInput;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        lastMove = new Vector2(0, 0);
     }
 
     
@@ -22,18 +30,35 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         Jump();
+
+        if (rb.velocity.magnitude > 0.1)
+        {
+            anim.SetBool("PlayerMoving", true);
+            anim.SetFloat("Move X", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("ClampMoveX", Input.GetAxisRaw("Horizontal"));
+            lastMove = moveInput;
+        }
+        else
+        {
+            anim.SetBool("PlayerMoving", false);
+        }
+
     }
 
     void Move()
     {
+        
+
         if(isGrounded)
         {
-            float x = Input.GetAxisRaw("Horizontal");
-            float moveBy = x * speed;
-            rb.velocity = new Vector2(moveBy, rb.velocity.y);
+          float x = Input.GetAxisRaw("Horizontal");
+          float moveBy = x * speed;
+          rb.velocity = new Vector2(moveBy, rb.velocity.y);
         }
+        
+        
     }
-
+    
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Z) && isGrounded)
