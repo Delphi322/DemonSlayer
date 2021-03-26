37d5db3d7a187a295722148c6db38bba6f9c57bf
Blossom13 <rawlinson322@gmail.com>
2021-03-26 12:27:13 +0000

    Added sword sub weapon animation and started work on sub weapon ammo system

---
+++ Assets/Scripts/AmmoDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoDisplay : MonoBehaviour
+{
+
+    public int ammo;
+
+    public bool isSub;
+
+    public Text ammoDisplay;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        ammoDisplay.text = ammo.ToString();
+        if (Input.GetKeyDown(KeyCode.F) && !isSub && ammo > 0)
+        {
+            isSub = true;
+            ammo--;
+            isSub = false;
+        }
+    }
+}
