d294fea81266079c8e429b8d83c2e0bbfd7b3fdf
DelphiFeverpitch <rawlinson322@gmail.com>
2021-04-20 07:05:40 +0100

    a

--- Assets/Scripts/CameraFollow2D.cs
+++ Assets/Scripts/CameraFollow2D.cs
@@ -5,54 +5,54 @@ using UnityEngine;
 
 public class CameraFollow2D : MonoBehaviour
 {
-    [SerializeField]
-    GameObject player;
+    /* [SerializeField]
+     GameObject player;
 
-    [SerializeField]
-    float timeOffSet;
+     [SerializeField]
+     float timeOffSet;
 
-    [SerializeField]
-    Vector2 posOffSet;
+     [SerializeField]
+     Vector2 posOffSet;
 
-    [SerializeField]
-    float leftLimit;
-    [SerializeField]
-    float rightLimit;
-    [SerializeField]
-    float bottomLimit;
-    [SerializeField]
-    float topLimit;
+     [SerializeField]
+     float leftLimit;
+     [SerializeField]
+     float rightLimit;
+     [SerializeField]
+     float bottomLimit;
+     [SerializeField]
+     float topLimit;
 
-    void Update()
-    {
-        Vector3 startPos = transform.position;
+     void Update()
+     {
+         Vector3 startPos = transform.position;
 
-        Vector3 endPos = player.transform.position;
+         Vector3 endPos = player.transform.position;
 
-        endPos.x += posOffSet.x;
-        endPos.y += posOffSet.y;
-        endPos.z = -10;
+         endPos.x += posOffSet.x;
+         endPos.y += posOffSet.y;
+         endPos.z = -10;
 
-        transform.position = Vector3.Lerp(startPos, endPos, timeOffSet * Time.deltaTime);
-
-
-        transform.position = new Vector3
-        (
-         Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
-         Mathf.Clamp(transform.position.x, topLimit, bottomLimit),
-         transform.position.z
-        );
-
-    }
+         transform.position = Vector3.Lerp(startPos, endPos, timeOffSet * Time.deltaTime);
 
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
-        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
-        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
-        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
-    }
+         transform.position = new Vector3
+         (
+          Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+          Mathf.Clamp(transform.position.x, topLimit, bottomLimit),
+          transform.position.z
+         );
 
+     }
+
+
+     private void OnDrawGizmos()
+     {
+         Gizmos.color = Color.red;
+         Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+         Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+         Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+         Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+     }
+    */
 }
